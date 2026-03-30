878df69fc7198ed43b0ac811c2acb83b96fd76b1
adamsnode <adamsnode@gmail.com>
2026-03-30 16:40:16 +0200

    dodano mozliwosc zakonczenia programu jako opcja numer 4.

--- Program.cs
+++ Program.cs
@@ -6,51 +6,62 @@ class Program
 
     static void Main(string[] args) // <- to jest metoda, ona należy do klasy
     {
-        Console.WriteLine("Ile masz lat?");
+    for (int loop = 0; loop < 5; loop++)
+    {    
+        Console.WriteLine("Ile masz lat? - Masz 5 prób.");
         int wiek = int.Parse(Console.ReadLine()); //integer czyli liczby całkowie "Z". Czytaj z konsoli i konwertuj tekst na liczbę całkowitą.
-
         if (wiek >= 18) //jezeli zmienna wiek jest większa lub równa 18 to wykonaj instrukcję
         {
             Console.WriteLine("Może być, wchodzisz.");
-            Console.WriteLine("Wybierz działanie:");
-            Console.WriteLine("1 - Dodawanie");
-            Console.WriteLine("2 - Mnożenie");
-            Console.WriteLine("3 - sprawdź czy liczba jest parzysta");
-            int wybor = int.Parse(Console.ReadLine());
 
-            if (wybor == 1) // dodawanie
+            for ( ; ;)
             {
-                Console.WriteLine("Dodawanie. Podaj pierwszą liczbe i wciśniej enter:");
-                int a = int.Parse(Console.ReadLine());
-                Console.WriteLine("Podaj drugą liczbę i wcisnij enter:");
-                int b = int.Parse(Console.ReadLine());
-                Console.WriteLine("Suma:");
-                int wynik_dodawania = Dodawanie(a, b);
-                Console.WriteLine(wynik_dodawania);
-            } else if (wybor == 2) // mnozenie
-            {
-                Console.WriteLine("Mnożenie. Poadaj pierwszą liczbę i naciśnij enter:");
-                int a = int.Parse(Console.ReadLine());
-                Console.WriteLine("Podaj drugą liczbę i naciśnij enter:");
-                int b = int.Parse(Console.ReadLine());
-                int wynik_mnozenia = Mnozenie(a, b);
-                Console.WriteLine($"Iloczyn: {wynik_mnozenia}");
-            }  else if (wybor == 3) // czy parzysta
-            {
-                Console.WriteLine("Czy liczba parzysta? Podaj liczbę i wciśnij enter:");
-                int n = int.Parse(Console.ReadLine());
-                bool tf_parzysta = CzyParzysta(n);
-                if (tf_parzysta == true)
+                Console.WriteLine("Wybierz działanie:");
+                Console.WriteLine("1 - Dodawanie");
+                Console.WriteLine("2 - Mnożenie");
+                Console.WriteLine("3 - Sprawdź czy liczba jest parzysta");
+                Console.WriteLine("4 - Zakończ program");
+                int wybor = int.Parse(Console.ReadLine());
+
+                if (wybor == 1) // dodawanie
                 {
-                  Console.WriteLine("Wynik: Parzysta");
-                } else
-                { Console.WriteLine("Wynik: Nieparzysta"); }
-            }  
+                    Console.WriteLine("Dodawanie. Podaj pierwszą liczbe i wciśniej enter:");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj drugą liczbę i wcisnij enter:");
+                    int b = int.Parse(Console.ReadLine());
+                    int wynik_dodawania = Dodawanie(a, b);
+                    Console.WriteLine($"Suma: {wynik_dodawania}");
+                    
+                } else if (wybor == 2) // mnozenie
+                {
+                    Console.WriteLine("Mnożenie. Poadaj pierwszą liczbę i naciśnij enter:");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj drugą liczbę i naciśnij enter:");
+                    int b = int.Parse(Console.ReadLine());
+                    int wynik_mnozenia = Mnozenie(a, b);
+                    Console.WriteLine($"Iloczyn: {wynik_mnozenia}");
+                }  else if (wybor == 3) // czy parzysta
+                {
+                    Console.WriteLine("Czy liczba parzysta? Podaj liczbę i wciśnij enter:");
+                    int n = int.Parse(Console.ReadLine());
+                    bool tf_parzysta = CzyParzysta(n);
+                    if (tf_parzysta == true)
+                    {
+                    Console.WriteLine("Wynik: Parzysta");
+                    } else
+                    { Console.WriteLine("Wynik: Nieparzysta"); }
+                }  else if (wybor == 4) // zakończ program
+                    {
+                        Environment.Exit(0);
+                    }
+            }    
         } else 
             {
                 Console.WriteLine("Dla Ciebie zamknięte.");
             }      
     }
+    }
+
 
 static int Dodawanie(int a, int b)
 {
