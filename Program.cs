56365d9eb48fe6c916f3dce4832c18614a8b2142
adamsnode <adamsnode@gmail.com>
2026-03-30 12:26:22 +0200

    Dodano weryfikację wieku w instrukcji warunkowej if oraz else

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ile masz lat?");
+        int wiek = int.Parse(Console.ReadLine()); //integer czyli liczby całkowie "Z". Czytaj z konsoli i konwertuj tekst na liczbę całkowitą.
+        {         
+            if (wiek >= 18) //jezeli zmienna wiek jest większa lub równa 18 to wykonaj instrukcję
+            {
+                Console.WriteLine("Może być, wchodzisz.");
+            } else //jeżeli inaczej, to wykonaj instrukcję
+            {
+                Console.WriteLine("Dla Ciebie zamknięte.");
+            }
+        }
+        
+    }
+}
